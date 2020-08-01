79e534c0b4827fb517b1ce8ca8f8436b5aba8722
zhouhuan <zsean21598@163.com>
2020-08-01 16:46:35 +0800

    tab切换页面

---
+++ src/assets/global-style.js
@@ -0,0 +1,23 @@
+const extendClick = () => {
+  return `
+      position: relative;
+      &:before {
+        content: '';
+        position: absolute;
+        top: -10px; bottom: -10px; left: -10px; right: -10px;
+      };
+    `;
+};
+// 一行文字溢出部分用... 代替
+const noWrap = () => {
+  return `
+      text-overflow: ellipsis;
+      overflow: hidden;
+      white-space: nowrap;
+    `;
+};
+
+export default {
+  extendClick,
+  noWrap,
+};
